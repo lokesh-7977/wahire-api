e1893bce7ffaa2457512ab8f5950a6fa320751cd
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2025-02-24 23:09:20 +0530

    refactor:add port

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from app.config.database import init_db
 from app.routes.user_routes import router as user_router
@@ -13,3 +14,6 @@ async def root():
     return {"message": "Welcome to the WAHIRE API"}
 
 app.include_router(user_router, prefix="/users", tags=["Users"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
