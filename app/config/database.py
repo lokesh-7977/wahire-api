45dbaf80253309796ce850979a2fccabdb324653
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2025-02-24 23:06:45 +0530

    feat:add database initialization

--- app/config/database.py
+++ app/config/database.py
@@ -1,13 +1,18 @@
-from beanie import init_beanie
-import motor.motor_asyncio
+from beanie import init_beanie 
+import motor.motor_asyncio 
+from pymongo.uri_parser import parse_uri  
 from app.models.user_model import User
 from app.config.settings import Settings
 
 settings = Settings()
 
 async def init_db():
+    """Initialize MongoDB connection with Beanie."""
     client = motor.motor_asyncio.AsyncIOMotorClient(settings.database_uri)
-    database_name = settings.database_uri.rsplit("/", 1)[-1].split("?")[0]
-    database = client[database_name]
 
+    parsed_uri = parse_uri(settings.database_uri)
+    database_name = parsed_uri["database"]
+
+    database = client[database_name]
+    
     await init_beanie(database, document_models=[User])
